deb8bda90976de451b0f95c2dd5f9efce94ba1ff
jamescross813 <crossjames813@gmail.com>
2022-08-30 18:04:52 -0500

    data imported and set as state and prop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import List from "./List";
+import data from "./data"
+import React, {useState} from 'react'
 
 function App() {
+  const [info, setInfo] = useState(data)
+  console.log(info)
   return (
     <main>
       <section className="container">
 
         <h3>0 birthdays today</h3>
-        <List/>
+        <List info={info} />
         <button>Clear</button>
       </section>
     </main>
