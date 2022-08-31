a64dab6deb447112a32e63a74886c8014b585231
jamescross813 <crossjames813@gmail.com>
2022-08-30 20:24:08 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <main>
       <section className="container">
-
         <h3>0 birthdays today</h3>
         <List info={info} />
         <button onClick={()=>{console.log("clicked me!")}}>Clear</button>
