b9f7a5d4c7845956e3a828946ecca8873f8039aa
jamescross813 <crossjames813@gmail.com>
2022-08-30 20:27:54 -0500

    added date to data list

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ export default [
     {
       id: 1,
       name: 'Bertie Yates',
+      date: "8/31/1988",
       age: 29,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
@@ -9,6 +10,7 @@ export default [
     {
       id: 2,
       name: 'Hester Hogan',
+      date: "8/30/1988",
       age: 32,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
@@ -16,6 +18,7 @@ export default [
     {
       id: 3,
       name: 'Larry Little',
+      date: "1/14/1988",
       age: 36,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
@@ -23,6 +26,7 @@ export default [
     {
       id: 4,
       name: 'Sean Walsh',
+      date: "12/14/1988",
       age: 34,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
@@ -30,6 +34,7 @@ export default [
     {
       id: 5,
       name: 'Lola Gardner',
+      date: "12/13/1988",
       age: 29,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
