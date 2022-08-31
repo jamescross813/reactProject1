80eb123098d1dc2e28aa5fb7c852967cc5d82126
jamescross813 <crossjames813@gmail.com>
2022-08-30 20:33:32 -0500

    moved content from app to listcontainer

--- src/containers/ListContainer.js
+++ src/containers/ListContainer.js
@@ -8,7 +8,13 @@ class ListContainer extends Component{
         const [info, setInfo] = useState(data);
         return(
             <div>
-                <List/>
+                <main>
+                    <section className="container">
+                        <h3>{info.length()} birthdays today</h3>
+                        <List info={info} />
+                        <button onClick={()=>{console.log("clicked me!")}}>Clear</button>
+                    </section>
+                </main>
             </div>
         )
     }
