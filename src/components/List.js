d45d7355487f2be47fd85d59f1850b66de7fc922
jamescross813 <crossjames813@gmail.com>
2022-08-30 21:44:11 -0500

    map and render info

--- src/components/List.js
+++ src/components/List.js
@@ -2,10 +2,25 @@ import React from 'react';
 
 const List = ({info}) => {
 
-  
+  let infoList=()=>{
+    return(
+      info.map((i)=>{ 
+        const {id, name, age, image, date} = i
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        )
+      })
+    )
+  }
   return (
     <>
-      
+      {infoList()}
       
     </>
   );
