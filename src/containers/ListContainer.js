0251e6bea6a646f1f6e51df54c1c4a14e0fee38b
jamescross813 <crossjames813@gmail.com>
2022-08-30 20:31:34 -0500

    list container added for single responsibility

---
+++ src/containers/ListContainer.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import List from "../components/List";
+
+class ListContainer extends Component{
+    render(){
+        return(
+            <div>
+                <List/>
+            </div>
+        )
+    }
+}
+
+export default ListContainer
