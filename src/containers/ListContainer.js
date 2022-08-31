3fa2ae5fa424f9274b634ac722b983e508a5c09b
jamescross813 <crossjames813@gmail.com>
2022-08-30 20:36:59 -0500

    wip commented out unused info temp

--- src/containers/ListContainer.js
+++ src/containers/ListContainer.js
@@ -5,17 +5,16 @@ import List from "../components/List";
 class ListContainer extends Component{
     
     render(){
-        const [info, setInfo] = useState(data);
+        // const [info, setInfo] = useState(data);
+        const info = "Hello"
         return(
-            <div>
-                <main>
-                    <section className="container">
-                        <h3>{info.length()} birthdays today</h3>
+            <>
+                <section className="container">
+                        <h3>0 birthdays today</h3>
                         <List info={info} />
                         <button onClick={()=>{console.log("clicked me!")}}>Clear</button>
-                    </section>
-                </main>
-            </div>
+                </section>
+            </>
         )
     }
 }
