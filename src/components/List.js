7c0185c2ae2ce3bdde5de85822c94418e2540195
jamescross813 <crossjames813@gmail.com>
2022-08-30 21:49:45 -0500

    render function written

--- src/components/List.js
+++ src/components/List.js
@@ -2,20 +2,24 @@ import React from 'react';
 
 const List = ({info}) => {
 
+  
   let infoList=()=>{
     return(
       info.map((i)=>{ 
-        const {id, name, age, image, date} = i
-        return (
-          <article key={id} className='person'>
+        const {id, name, age, image, date} = i;       
+        return toRender(id, name, age, image, date) 
+      })
+    )
+  }
+
+  let toRender =(id, name, age, image, date)=>{
+    return(<article key={id} className='person'>
             <img src={image} alt={name} />
             <div>
               <h4>{name}</h4>
               <p>{age} years</p>
             </div>
           </article>
-        )
-      })
     )
   }
   return (
